503194db83b07c81820bb0f08a92db64cd563089
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2025-05-26 16:29:22 +0300

    finished team page

--- app/crew/[team]/page.tsx
+++ app/crew/[team]/page.tsx
@@ -11,8 +11,8 @@ export default function TeamPage ({params}: {params:{team: string}}) {
         <div className="
         bg-[url(../public/images/crew/background-crew-mobile.jpg)] 
         md:bg-[url(../public/images/crew/background-crew-tablet.jpg)]
-        xl:bg-[url(../public/images/crew/background-crew-desktop.jpg)] bg-cover bg-no-repeat items-center justify-items-center min-h-screen w-full p-8 md:pr-10 md:pl-10 md:pt-10 md:pb-0">
-        <div className='pt-6 md:pt-10 pb-6 w-full mt-[56px] xl:mt-[144px]'>
+        xl:bg-[url(../public/images/crew/background-crew-desktop.jpg)] bg-cover bg-no-repeat items-center justify-items-center min-h-screen w-full p-8 md:pr-10 md:pl-10 md:pt-10 md:pb-0 xl:pr-[165px] xl:pl-[165px] xl:pb-12">
+        <div className='pt-6 md:pt-10 xl:pt-[144px] pb-6 w-full mt-[56px] xl:mt-0'>
         <p className="font-barlow tracking-[3px] flex md:justify-start md:items-start text-base md:text-[20px]/10 xl:text-[28px] font-[400]  justify-center items-center">
             <span className="mr-6 text-gray-400">02</span>MEET YOUR CREW
         </p>
--- components/memberTeam/memberTeam.tsx
+++ components/memberTeam/memberTeam.tsx
@@ -14,38 +14,40 @@ export default function MemberTeam ({team} : CrewProps) {
     return (
         <>
         {team.map((item) => 
-        <div  key={item.id}>
-        <div className="pt-10 pb-10 flex flex-col justify-center items-center mb-6">
-                <h3 className=" font-bellefair text-lg md:text-2xl mb-2 md:mb-4 text-gray-400">{item.role}</h3>
-                <h1 className="font-bellefair tracking-normal text-white text-2xl md:text-[40px] mb-6">{item.name}</h1>
-                <p className="w-[512px] font-barlow_norm text-[16px] text-center text-[#D0D6F9]">{item.bio}</p>
+        <div  key={item.id} className="xl:flex ">
+            <div className="xl:mr-8">
+        <div className="xl:w-[539px] xl:h-[631px] pt-10 pb-10 xl:p-0 flex flex-col justify-center xl:justify-center items-center xl:items-start mb-6 xl:mb-10 ">
+                <h3 className=" font-bellefair text-lg md:text-2xl  xl:text-[32px] mb-2 md:mb-4 xl:mb-8 text-gray-400">{item.role}</h3>
+                <h1 className="whitespace-nowrap font-bellefair tracking-normal h-auto text-white text-2xl md:text-[40px] xl:text-[56px] mb-6 xl:mb-8">{item.name}</h1>
+                <p className="md:w-[512px] font-barlow_norm text-[16px] xl:text-lg text-center xl:text-start text-[#D0D6F9]">{item.bio}</p>
         </div>
-      <ul className="flex gap-[16px] justify-center items-center mb-[37px]">
+      <ul className="flex gap-[16px] xl:gap-10 justify-center xl:justify-start  items-center xl:items-start mb-[37px] xl:mb-[48px]">
         {pathname.includes('c-1') ?  <li>
-        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] h-[10px]"/>
+        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] xl:w-4 h-[10px] xl:h-4"/>
         </li> :   <li>
-            <Link href="/crew/c-1" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px]"/>
+            <Link href="/crew/c-1" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px] xl:w-4 xl:h-4"/>
         </li>}
 
         {pathname.includes('c-2') ?  <li>
-        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] h-[10px]"/>
+        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] h-[10px] xl:w-4 xl:h-4"/>
         </li> :   <li>
-            <Link href="/crew/c-2" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px]"/>
+            <Link href="/crew/c-2" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px] xl:w-4 xl:h-4"/>
         </li>}
         {pathname.includes('c-3') ?  <li>
-        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] h-[10px]"/>
+        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] h-[10px] xl:w-4 xl:h-4"/>
         </li> :   <li>
-            <Link href="/crew/c-3" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px]"/>
+            <Link href="/crew/c-3" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px] xl:w-4 xl:h-4"/>
         </li>}
 
         {pathname.includes('c-4') ?  <li>
-        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] h-[10px]"/>
+        <Link href={`${pathname}`} color="#fff" className="flex rounded-[50%] bg-white w-[10px] h-[10px] xl:w-4 xl:h-4"/>
         </li> :   <li>
-            <Link href="/crew/c-4" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px]"/>
+            <Link href="/crew/c-4" className="flex rounded-[50%] bg-[#979797] w-[10px] h-[10px] xl:w-4 xl:h-4"/>
         </li>}
-           </ul>    
-          <div className=" flex justify-center items-center">
-            <Image src={item.images.png} alt={item.name} priority width={271} height={340} className="md:w-[446px]"/>
+           </ul>  
+           </div>  
+          <div className=" flex justify-center items-center xl:h-[676px] xl:mt-[30px] xl:mb-[30px]">
+            <Image src={item.images.png} alt={item.name} priority width={271} height={340} className="md:w-[446px] xl:w-[540px] xl:h-full "/>
             </div> 
         </div>)}
         </>
